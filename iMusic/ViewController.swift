d6b2454c4b04a0b49b5a46d547661be57cf91dc3
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-12-10 16:28:57 +0300

    Initial Commit

---
+++ iMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusic
+//
+//  Created by Дмитрий Кириченко on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
