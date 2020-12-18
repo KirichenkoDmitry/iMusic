32f34615104f925b653ca98cd042e9c2bd0eadf7
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-12-18 18:12:00 +0300

    Implement UI Create Models

---
+++ iMusic/MainTapBarController.swift
@@ -0,0 +1,35 @@
+//
+//  MainTapBarController.swift
+//  iMusic
+//
+//  Created by Дмитрий Кириченко on 10.12.2020.
+//
+
+import UIKit
+
+class MainTapBarController: UITabBarController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-search-5nav-icon"), title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon"), title: "Library")
+        ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+    
+}
---
+++ iMusic/NetworkService.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkService.swift
+//  iMusic
+//
+//  Created by Дмитрий Кириченко on 11.12.2020.
+//
+
+import UIKit
+import Alamofire
+
+class NetworkService {
+    
+    func fetchTracks(searchText: String, complition: @escaping (SearchResponse?) -> Void) {
+        let url = "https://itunes.apple.com/search"
+        let parameters = ["term": "\(searchText)",
+                          "limit": "10",
+                          "media": "music"
+        ]
+        
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponce) in
+            if let error = dataResponce.error {
+                print("Error recieved requestion data: \(error.localizedDescription)")
+                complition(nil)
+                return
+            }
+            
+            guard let data = dataResponce.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                print("objects: ", objects)
+                complition(objects)
+                
+                
+            } catch let jsonError {
+                print("Failed to decode JSON", jsonError)
+                complition(nil)
+            }
+            
+//                let someString = String(data: data, encoding: .utf8)
+//                print(someString ?? "")
+        }
+    }
+}
