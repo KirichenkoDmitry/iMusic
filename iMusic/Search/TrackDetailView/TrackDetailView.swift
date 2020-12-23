9c1f9fe002884d5dcc65bc7256ca7dba2456b07b
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-12-24 00:19:22 +0300

    Fix bugs with images in TrackDetailView.xib

--- iMusic/Search/TrackDetailView/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -9,7 +9,38 @@ import UIKit
 
 class TrackDetailView: UIView {
     
-    override class func awakeFromNib() {
+    @IBOutlet var trackImageView: UIImageView!
+    @IBOutlet var currentTimeSlider: UISlider!
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var trackTitleLabel: UILabel!
+    @IBOutlet var authorTitleLabel: UILabel!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var volumeSlider: UISlider!
+    
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
+        
+        trackImageView.backgroundColor = .red
+    
     }
+    
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    @IBAction func playPauseAction(_ sender: Any) {
+    }
+    
 }
